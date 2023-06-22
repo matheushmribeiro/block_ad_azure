948c5bb382b6342b248bcf056b78003d9b41edb0
Matheus Ribeiro <130484612+matheushmribeiro@users.noreply.github.com>
2023-06-22 16:51:12 -0300

    Update Scriptbloqueio.ps1
    melhorado a saida de logs e automatizado a limpeza do arquivo de cadastro de bloqueios
    
    Signed-off-by: Matheus Ribeiro <130484612+matheushmribeiro@users.noreply.github.com>

--- Scriptbloqueio.ps1
+++ Scriptbloqueio.ps1
@@ -1,22 +1,23 @@
 #gerando logs
-start-transcript -Path "caminhodasuamaquina\log.txt" -append -NoClobber
-Write-Host iniciando o script: (Get-Date)   
+$filepath = "local dos logs"
+Write-Output "--------------------------------------------------------------------------------------------------------------------------------------------------------"  | Out-File -FilePath $filepath -Append
+Write-Output "iniciando o script: BLOQUEIO DE USUARIOS" (Get-Date) | Out-File -FilePath $filepath -Append
 
-#setando as variaveis usuario e senha cripto do azure ad
+#setando variaveis do MSol
 
-$username = "seu_login@dominio.com" 
-$password = Get-Content "caminhodasuamaquina\cripto1.txt" |ConvertTo-SecureString 
+$username = "login_aqui" 
+$password = Get-Content "localização_da_senha_encript" |ConvertTo-SecureString 
 
-#setando as variaveis usuario e senha cripto do adds
+#setando as variaveis usuario e senha cripto do ad
 
-$usernamead = "seuusuario"
-$passwordad = Get-Content "caminhodasuamaquina\cripto2.txt" |ConvertTo-SecureString
+$usernamead = "login_aqui"
+$passwordad = Get-Content "localização_da_senha_encript" |ConvertTo-SecureString
 
-#definindo a variavel cred e credad para automatizar o login
+#definindo a variavel cred para automatizar o login
 
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
 $credad = new-object -typename System.Management.Automation.PSCredential -argumentlist $usernamead, $passwordad
-#testando a Conexão com o MSOL:
+#Testando a conexão com o MSOL:
 Connect-MsolService -Credential $cred
 
 #Definir variáveis
@@ -25,9 +26,9 @@ $Data = ""
 $Loginad = ""
 
 #Leitura do arquivo CSV
-$csv = Import-Csv 'caminhodasuamaquina\colaboradores-bloqueio.csv'
+$csv = Import-Csv 'local do csv'
 
-#Criação da tabela
+#Criação da tabela e ocultando a saida dela
 $csv | Format-Table | Out-Null
 
 #Atribuição de valores às variáveis
@@ -44,14 +45,81 @@ if ($Data -eq $Date)
 {  
     #Realizar o bloqueio do usuário no Azure AD
     Set-MsolUser -UserPrincipalName $Login -BlockCredential $true
-    #Desabilita o usuario no adds
-     Disable-ADAccount -Credential $credad $Loginad -Verbose 
+    #Desabilita o usuario
+     Write-Output "Operação De bloqueio No AD:" | Out-File -FilePath $filepath -Append
+     Disable-ADAccount -Credential $credad $Loginad -Verbose 4>&1 | Out-File -FilePath $filepath -Append
 
 #print dos logins bloqueados
- Write-Host Usuarios bloqueados: $login $Data $Loginad
+
+
+    Write-Output "Usuario bloqueado No Azure:" $login,$Data,$Loginad | Out-File -FilePath $filepath -Append
+    Write-Output " 
+" | Out-File -FilePath $filepath -Append
+
 }
 }
-#para de gerar log
-stop-transcript
+    Write-Output "Finalizando o script:" (Get-Date) | Out-File -FilePath $filepath -Append
+
+
+    Write-Output "--------------------------------------------------------------------------------------------------------------------------------------------------------"  | Out-File -FilePath $filepath -Append
+
+
+#limpeza automatica do csv de cadastro de informações
+
+
+#print no arquivo 
+        Write-Output "Verificando a lista"  | Out-File -FilePath $filepath -Append
+# Define o caminho do arquivo CSV
+$csvPath = 'local do csv'
+$saida = "local do csv"
+# Carrega o arquivo CSV
+$csv = Import-Csv $csvPath
+
+# Define o formato de data das linhas
+$dateFormat = "dd/MM/yy"
+
+# Define a data limite de 10 dias antes da data atual
+$limitDate = (Get-Date).AddDays(-10)
+
+# Formata as colunas e tabela as informações
+$csv | Select-Object @{Name="Login";Expression={$_.Login}},
+                    @{Name="Data";Expression={[datetime]::ParseExact($_.Data, $dateFormat, $null)}},
+                    @{Name="Loginad";Expression={$_.Loginad}} | Format-Table | Out-Null
+
+# Define a variável booleana como false
+$foundMatch ="false"
+
+# Faz a iteração pelas linhas e verifica as datas
+foreach ($row in $csv) {
+    $rowDate = [datetime]::ParseExact($row.data, $dateFormat, $null)
+    
+    # Verifica se a data da linha é anterior à data limite
+    if ($rowDate -lt $limitDate) {
+         Write-Output $login $data  | Out-File -FilePath $filepath -Append
+
+        # Remove a linha do arquivo CSV
+        $csv = $csv | Where-Object { $_.Data -ne $row.Data }
+        # Altere a variável booleana como true para indicar que alguma linha correspondeu à regra
+        $foundMatch ="true"
+
+    }
+     
+}
+# Imprime uma mensagem se a variável $foundMatch ainda estiver como false
+if ($foundMatch -eq "false" ) {
+         Write-Output   "Nenhum login corresponde à regra" | Out-File -FilePath $filepath -Append
+}
+
+
+# Escreve as linhas restantes para o mesmo arquivo CSV  
+$csv | ConvertTo-Csv -Delimiter "," -NoTypeInformation  | ForEach-Object { $_.Replace('"','') } | Out-File -FilePath $saida
+
+#print no arquivo 
+        Write-Output "Finalizando a Limpeza realizada na lista"  | Out-File -FilePath $filepath -Append
+
+        Write-Output "--------------------------------------------------------------------------------------------------------------------------------------------------------"  | Out-File -FilePath $filepath -Append
+
+
+
 
 exit
